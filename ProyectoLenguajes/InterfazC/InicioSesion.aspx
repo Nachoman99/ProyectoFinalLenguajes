1287db0a081c412b52af3312b6a3b7948737b383
Juan Carlos Valverde Araya <juan.valverde@ucrso.info>
2020-06-22 16:11:08 -0600

    Inicio Sesion Interfaz

---
+++ ProyectoLenguajes/InterfazC/InicioSesion.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InicioSesion.aspx.cs" Inherits="InterfazC.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align: center">
+            Bienvenido<br />
+            <br />
+            Usuario<br />
+            <asp:TextBox ID="tbUser" runat="server" Width="180px"></asp:TextBox>
+            <br />
+            Contraseña<br />
+            <asp:TextBox ID="tbPass" runat="server" Width="180px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btSign" runat="server" Text="Iniciar Sesión" />
+&nbsp;O
+            <asp:Button ID="btRegist" runat="server" Text="Registrarse" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProyectoLenguajes/InterfazC/InicioSesion.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace InterfazC
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control tbUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbUser;
+
+        /// <summary>
+        /// Control tbPass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbPass;
+
+        /// <summary>
+        /// Control btSign.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btSign;
+
+        /// <summary>
+        /// Control btRegist.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btRegist;
+    }
+}
